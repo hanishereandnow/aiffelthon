a535ed7cd8c41d4226f9f79a684b513497e2b373
hanishereandnow <106140951+hanishereandnow@users.noreply.github.com>
2023-03-03 11:58:41 +0900

    Delete python directory

--- python/unit24-1.py
+++
@@ -1,7 +0,0 @@
-# 24.6 심사문제: 높은 가격순으로 출력하기
-
-price = list(map(int, input().split(';')))
-price.sort(reverse=True)    # 오름차순으로 정렬
-
-for i in range(len(price)):
-    print('{0:>9,}'.format(price[i]))
--- python/unit32.5.py
+++
@@ -1,8 +0,0 @@
-files = input().split()
-newfiles = []
-
-for i in range(len(files)):
-   if '.' in files:
-       newfiles.append(files[i].split('.'))
-
-print(newfiles)
